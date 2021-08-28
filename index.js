41998b0d2dc9ab1612d92e3a995286da697e54e7
CalogeroCantone <calolavendetta@gmail.com>
2021-08-28 07:57:04 +0200

    Delete date directory

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const execSync = require('child_process').execSync
 
 try {
 
   const targetDir = core.getInput('dir');
-  console.log(`Directory to backup: ${targetDir}!`);
+  console.log(`Directory to backup: ${targetDir}`);
 
   const outputDir = targetDir.replace('/static/laboratory', '/backup')
-  console.log(`Output directory: ${outputDir}!`);
-  
-  const time = (new Date()).getMilliseconds();
-  console.log(time)
+  console.log(`Output directory: ${outputDir}`);
   
   execSync(`mkdir -p ${targetDir}`, { encoding: 'utf-8' })
-  execSync(`mkdir -p ${outputDir}/${time}`, { encoding: 'utf-8' })
-  const output = execSync(`cp ${targetDir}/* ${outputDir}/${time}`, { encoding: 'utf-8' })
+
+  execSync(`rm -rf ${outputDir}`, { encoding: 'utf-8' })
+  execSync(`mkdir -p ${outputDir}`, { encoding: 'utf-8' })
+
+  const output = execSync(`cp ${targetDir}/* ${outputDir}`, { encoding: 'utf-8' })
   console.log(output)
 
 } catch (error) {
